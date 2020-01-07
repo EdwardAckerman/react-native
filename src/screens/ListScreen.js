a02f364b0ccace5059124283720e16b6c5fd668c
Eduardo Silva <eduardo.silva@fireworkweb.com>
2020-01-07 12:44:11 -0300

    adding buttons for different screens

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,44 +3,38 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
   const friends = [
-    {
-      name: 'Philip',
-      key: '1',
-    },
-    {
-      name: 'Carlos',
-      key: '2',
-    },
-    {
-      name: 'Weesley',
-      key: '3',
-    },
-    {
-      name: 'Real',
-      key: '4',
-    },
-    {
-      name: 'JS',
-      key: '5',
-    },
-    {
-      name: 'Daniel',
-      key: '6',
-    },
+    { name: 'Philip', age: 25 },
+    { name: 'Carlos', age: 25 },
+    { name: 'Weesley', age: 25 },
+    { name: 'Real', age: 25 },
+    { name: 'JS', age: 25 },
+    { name: 'Daniel', age: 25 },
+    { name: 'Gabe', age: 25 },
+    { name: 'Scroll', age: 25 },
+    { name: 'Bottom', age: 25 },
   ];
 
   // keyExtractor={friend => friend.name} is an option when the object has no "keys";
   return (
     <FlatList
+      showsVerticalScrollIndicator={false}
       keyExtractor={friend => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text>{ item.name }</Text>
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
       }}
     ></FlatList>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 10,
+  },
+});
 
 export default ListScreen;
