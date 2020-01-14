5e3f37e68bc2862d2dc51c9ec0756e56d8fd477f
Eduardo Silva <eduardo.silva@fireworkweb.com>
2020-01-14 10:21:42 -0300

    displaying prop images

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = (props) => {
-return <Text>Show image of a {props.title} </Text>;
+  return <View>
+    <Image source={props.imageSource}></Image>
+    <Text>Show image of a {props.title}</Text>
+  </View>
 };
 
 const styles = StyleSheet.create({});
